7253ed121cbde49c3c8f78e6844ac388d56fd165
Abraham <ulrichouedraogo500@gmail.com>
2025-10-24 18:37:54 +0200

    mise en place vote en temps reel, avec upd kafka

---
+++ spark-streaming.py
@@ -0,0 +1,85 @@
+import pyspark
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
+from pyspark.sql.functions import sum as spark_sum
+from pyspark.sql import functions as F
+
+
+if __name__ == "__main__":
+
+    spark = pyspark.sql.SparkSession.builder.appName("real time voting eng")\
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1")\
+            .config("spark.jars.packages", "./postgresql-42.7.8.jar")\
+            .config("spark.sql.adaptive.enabled", "false")\
+        .getOrCreate()
+        
+    vote_schema = StructType([
+            StructField("voter_id", IntegerType(), True),
+            StructField("candidate_id", IntegerType(), True),
+            StructField("voting_time", TimestampType(), True),
+            StructField("voter_name", StringType(), True),
+            StructField("party", StringType(), True),
+            StructField("biography", StringType(), True),
+            StructField("campaign_promises", StringType(), True),
+            StructField("photo_url", StringType(), True),
+            StructField("date_of_bd", StringType(), True),
+            StructField("gender", StringType(), True),
+            StructField("nationality", StringType(), True),
+            StructField("address", StringType(), True),
+            StructField("email", StringType(), True),
+            StructField("picture_url", StringType(), True),
+            StructField("phone_number", StringType(), True),
+            StructField("vote", IntegerType(), True),
+        ])
+        
+    votes_df = spark \
+            .readStream \
+                .format("kafka") \
+                .option("kafka.bootstrap.servers", "localhost:9092") \
+                .option("subscribe", "votes_topic") \
+                .option("startingOffsets", "earliest") \
+                .load()\
+            .selectExpr("CAST(value AS STRING)")\
+            .select(from_json(col("value"), vote_schema).alias("data"))\
+            .select("data.*")
+            
+            
+    votes_df = votes_df.withColumn("voting_time", col("voting_time").cast(TimestampType()))\
+        .withColumn("vote", col("vote").cast(IntegerType()))
+        
+    enriched_votes_df = votes_df.withWatermark("voting_time", "1 minute")
+                
+            
+    #aggregate votes per candidate every 5 minutes
+    votes_per_candidates = enriched_votes_df.groupBy("candidate_id","photo_url")\
+        .agg(spark_sum('vote').alias('total_votes'))
+    
+    enriched_votes_df = enriched_votes_df.withColumn(
+    "country",
+    F.split(F.col("address"), ",")[-2].trim()
+)
+        
+    turnout_by_location_df = enriched_votes_df.groupBy("country").count().alias("total_votes")
+    
+    votes_per_candidate_to_kafka = votes_per_candidates.selectExpr('to_json(struct(*)) AS value') \
+        .writeStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "aggregated_votes_per_candidate") \
+        .option("checkpointLocation", "C:\Users\H P\git\RealTimeVotingEng\checkpoints\checkpoint1")\
+        .outputMode("update") \
+        .start()
+        
+    turnout_by_location_to_kafka = votes_per_candidates.selectExpr('to_json(struct(*)) AS value') \
+        .writeStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "aggregated_turnout_by_location") \
+        .option("checkpointLocation", "C:\Users\H P\git\RealTimeVotingEng\checkpoints\checkpoint2")\
+        .outputMode("update") \
+        .start()
+        
+    votes_per_candidate_to_kafka.awaitTermination()
+    turnout_by_location_to_kafka.awaitTermination()
+    
+    
